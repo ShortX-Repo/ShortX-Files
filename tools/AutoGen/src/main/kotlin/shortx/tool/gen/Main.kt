2afbc1308a9c594e4d275c84fd399312a36d8a9e
ShortX <shortx@163.com>
2023-07-19 17:36:31 +0800

    print abs path

--- tools/AutoGen/src/main/kotlin/shortx/tool/gen/Main.kt
+++ tools/AutoGen/src/main/kotlin/shortx/tool/gen/Main.kt
@@ -1,11 +1,12 @@
 package shortx.tool.gen
 
+import java.io.File
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
     val dir = args[0]
     Logger.enableDebug()
-    Logger.info("Dir: $dir")
+    Logger.info("Dir: ${File(dir).canonicalFile.absolutePath}")
     Gen.run(dir)
     exitProcess(0)
 }
