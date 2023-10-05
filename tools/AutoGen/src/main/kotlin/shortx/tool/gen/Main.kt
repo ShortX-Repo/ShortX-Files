79f35490ccb6b4670dee12793dd6b90cf6862d63
Tornaco <Tornaco@163.com>
2023-10-05 15:31:46 +0800

    fix build & update core

--- tools/AutoGen/src/main/kotlin/shortx/tool/gen/Main.kt
+++ tools/AutoGen/src/main/kotlin/shortx/tool/gen/Main.kt
@@ -1,8 +1,12 @@
 package shortx.tool.gen
 
+import tornaco.apps.shortx.core.util.logAdapter
 import java.io.File
 
 fun main(args: Array<String>) {
+    logAdapter = { _: Int, tag: String, msg: String ->
+        println("[$tag] $msg")
+    }
     val inputDir = args[0]
     Logger.enableDebug()
     Logger.info("inputDir: ${File(inputDir).canonicalFile.absolutePath}")
